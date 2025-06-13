f8408c74889b5cc7fbe08e3a03bec52fd5755606
Halil <ibayy93@gmail.com>
2025-06-13 11:57:47 -0700

    Remove .prettierignore file and update modal styles for consistency

--- vite-project/src/components/ModalWithForm/ModalWithForm.jsx
+++ vite-project/src/components/ModalWithForm/ModalWithForm.jsx
@@ -11,7 +11,7 @@ function ModalWithForm({
 }) {
   return (
     <div className={`modal ${isOpen ? "modal_opened" : ""}`}>
-      <div className="modal__content">
+      <div className="modal-form__content">
         <h2 className="modal__title">{title}</h2>
         <button
           onClick={closeActiveModal}
